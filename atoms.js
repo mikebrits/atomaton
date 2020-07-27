9434a415365effeeee4385feefe15dc94a9eb208
britsm <mikebrits@rentalcars.com>
2020-07-27 21:12:50 +0100

    Initial List of Atoms

---
+++ atoms.js
@@ -0,0 +1,32 @@
+export default {
+    typography: {
+        Text: '',
+        Link: '',
+    },
+    input: {
+        Button: '',
+        Checkbox: '',
+        Date: '',
+        Time: '',
+        DateTime: '',
+        Radio: '',
+        Select: '',
+        Slider: '',
+        Switch: '',
+        TextField: '',
+    },
+    elements: {
+        Box: '',
+        Divider: '',
+        Image: '',
+        OrderedList: '',
+        UnorderedList: '',
+        ListItem: '',
+        Table: '',
+        TableHead: '',
+        TableRow: '',
+        TableCell: '',
+        TableBody: '',
+        Progress: '',
+    }
+}
